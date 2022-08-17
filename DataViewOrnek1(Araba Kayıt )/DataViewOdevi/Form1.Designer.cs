010db7bfcd0cd9f8c71699876babfcb129ae5291
Yeliz <81921661+yelizsn@users.noreply.github.com>
2022-08-17 12:06:20 +0300

    Add files via upload

---
+++ DataViewOrnek1(Araba Kayıt )/DataViewOdevi/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DataViewOdevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_marka = new System.Windows.Forms.TextBox();
+            this.txt_model = new System.Windows.Forms.TextBox();
+            this.txt_beygirGucu = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(375, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Veri Ekle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(827, 286);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txt_marka
+            // 
+            this.txt_marka.BackColor = System.Drawing.Color.White;
+            this.txt_marka.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_marka.Location = new System.Drawing.Point(265, 319);
+            this.txt_marka.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_marka.Name = "txt_marka";
+            this.txt_marka.Size = new System.Drawing.Size(470, 46);
+            this.txt_marka.TabIndex = 3;
+            // 
+            // txt_model
+            // 
+            this.txt_model.BackColor = System.Drawing.Color.White;
+            this.txt_model.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_model.Location = new System.Drawing.Point(265, 361);
+            this.txt_model.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_model.Name = "txt_model";
+            this.txt_model.Size = new System.Drawing.Size(470, 46);
+            this.txt_model.TabIndex = 4;
+            // 
+            // txt_beygirGucu
+            // 
+            this.txt_beygirGucu.BackColor = System.Drawing.Color.White;
+            this.txt_beygirGucu.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_beygirGucu.Location = new System.Drawing.Point(265, 403);
+            this.txt_beygirGucu.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_beygirGucu.Name = "txt_beygirGucu";
+            this.txt_beygirGucu.Size = new System.Drawing.Size(470, 46);
+            this.txt_beygirGucu.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(132, 319);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Marka :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(133, 361);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 39);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Model :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(39, 410);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 39);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Beygir Gücü :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(851, 528);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_beygirGucu);
+            this.Controls.Add(this.txt_model);
+            this.Controls.Add(this.txt_marka);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Araba Verisi Ekleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_marka;
+        private System.Windows.Forms.TextBox txt_model;
+        private System.Windows.Forms.TextBox txt_beygirGucu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DataViewOrnek1(Araba Kayıt )/DataViewOdevi/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataViewOdevi
+{
+    public partial class Form1 : Form
+    {
+
+        DataView DtView;
+        DataSet DtSet;
+        OleDbDataAdapter DataAdapter;
+        OleDbCommand Komut;
+        string ConnectionString = "Provider=Microsoft.ACE.Oledb.12.0;Data Source=araclar.accdb"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            VeriGoruntule(); 
+        }
+        private void VeriGoruntule() 
+        {
+            using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+            {
+                DtView = new DataView();
+                DtSet = new DataSet();
+                Komut = new OleDbCommand("SELECT * FROM Araclar_Tablosu", baglanti); 
+                DataAdapter = new OleDbDataAdapter();  
+                DataAdapter.SelectCommand = Komut;
+                DataAdapter.Fill(DtSet);
+                DtView = new DataView(DtSet.Tables[0]); 
+                DtView.Sort = "Id"; 
+                dataGridView1.DataSource = DtView; 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRowView newRow = DtView.AddNew(); 
+                newRow["Marka"] = txt_marka.Text;
+                newRow["Model"] = txt_model.Text;
+                newRow["Beygir_Gucu"] = Convert.ToUInt16(txt_beygirGucu.Text);
+                newRow.EndEdit();
+                DtView.Sort = "Id";
+                using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+                { 
+                    OleDbCommand com = new OleDbCommand("INSERT INTO Araclar_Tablosu (Marka,Model,Beygir_Gucu) values (@Marka,@Model,@Beygir_Gucu)");
+                    com.Connection = baglanti;
+                    baglanti.Open();
+                    if (baglanti.State == ConnectionState.Open)
+                    {
+                        
+                        com.Parameters.Add("@Marka", OleDbType.VarChar).Value = DtView[0][1];           
+                        com.Parameters.Add("@Model", OleDbType.VarChar).Value = DtView[0][2];           
+                        com.Parameters.Add("@Beygir_Gucu", OleDbType.VarChar).Value = DtView[0][3];     
+
+                        try
+                        {
+                            com.ExecuteNonQuery(); 
+                            MessageBox.Show("Veri Eklendi");
+                            baglanti.Close();
+                            VeriGoruntule();
+                        }
+                        catch (OleDbException ex)
+                        {
+                            MessageBox.Show(ex.Source);
+                            baglanti.Close(); 
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bağlantı Hatası");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
